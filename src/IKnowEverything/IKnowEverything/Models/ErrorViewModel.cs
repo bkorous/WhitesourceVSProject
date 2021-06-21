4a1ede5f4ed4986933fa02ae821c3ac8ff953bc9
Brandon Korous <me@brandonkorous.com>
2021-06-21 14:10:18 -0700

    Add project

---
+++ src/IKnowEverything/IKnowEverything/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IKnowEverything.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
